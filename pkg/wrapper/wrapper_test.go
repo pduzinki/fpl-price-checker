239eb333a79cba9bf57493d47411dd8ea8d74fd6
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2022-12-18 15:39:48 +0100

    add wrapper tests

--- pkg/wrapper/wrapper_test.go
+++ pkg/wrapper/wrapper_test.go
@@ -1,11 +1,61 @@
 package wrapper
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
 
 func TestFetchData(t *testing.T) {
-	// TODO add test
-}
+	testcases := []struct {
+		name         string
+		statusCode   int
+		bodyFilePath string
+		expectErr    bool
+		wantErr      error
+	}{
+		{
+			name:         "sunny scenario",
+			statusCode:   http.StatusOK,
+			bodyFilePath: "./testdata/fetchdata.json",
+			wantErr:      nil,
+		},
+		{
+			name:         "too many requests",
+			statusCode:   http.StatusTooManyRequests,
+			bodyFilePath: "./testdata/fetchdata.json",
+			wantErr:      ErrHttpRequest{http.StatusTooManyRequests},
+		},
+	}
 
-func TestGetPlayers(t *testing.T) {
-	// TODO add test
+	for _, test := range testcases {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(test.statusCode)
+			w.Header().Set("Content-Type", "application/json")
+
+			f, err := os.ReadFile(test.bodyFilePath)
+			if err != nil {
+				t.Error(err)
+			}
+
+			w.Write(f)
+		}))
+		defer server.Close()
+
+		w := Wrapper{
+			client:  &http.Client{},
+			baseURL: server.URL,
+		}
+
+		type tmp struct {
+			Data int `json:"data"`
+		}
+		var data tmp
+
+		err := w.fetchData(w.baseURL, &data)
+		if err != test.wantErr {
+			t.Errorf("want: %d, got: %d", test.wantErr, err)
+		}
+	}
 }
