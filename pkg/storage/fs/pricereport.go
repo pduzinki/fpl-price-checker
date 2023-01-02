59c9eb54984ee85fd7b438aec000997d194d607a
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2023-01-02 20:34:48 +0100

    add price report repository

---
+++ pkg/storage/fs/pricereport.go
@@ -0,0 +1,68 @@
+package fs
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/pduzinki/fpl-price-checker/pkg/domain"
+	"github.com/pduzinki/fpl-price-checker/pkg/storage"
+)
+
+type PriceReportRepository struct {
+	folderPath string
+	sync.RWMutex
+}
+
+func NewPriceReportRepository(folderPath string) (*PriceReportRepository, error) {
+	err := os.MkdirAll(folderPath, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create price report data repository: %w", err)
+	}
+
+	return &PriceReportRepository{}, nil
+}
+
+func (pr *PriceReportRepository) Add(_ context.Context, date string, report domain.PriceChangeReport) error {
+	pr.Lock()
+	defer pr.Unlock()
+
+	if err := domain.ParseDate(date); err != nil {
+		return fmt.Errorf("fs.PriceReportRepository.Add failed to parse date: %w", err)
+	}
+
+	filename := filepath.Join(pr.folderPath, date)
+
+	if _, err := os.Stat(filename); err == nil {
+		return fmt.Errorf("fs.PriceReportRepository.Add failed: %w", storage.ErrDataAlreadyExists)
+	}
+
+	f, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("fs.PriceReportRepository.Add failed to create file: %w", err)
+	}
+	defer f.Close()
+
+	jsonReport, err := json.Marshal(report)
+	if err != nil {
+		return fmt.Errorf("fs.PriceReportRepository.Add failed to marshal data: %w", err)
+	}
+
+	if _, err := f.Write(jsonReport); err != nil {
+		return fmt.Errorf("fs.PriceReportRepository.Add failed to write data into file: %w", err)
+	}
+
+	return nil
+}
+
+func (pr *PriceReportRepository) GetByDate(_ context.Context, date string) (*domain.PriceChangeReport, error) {
+	pr.Lock()
+	defer pr.Unlock()
+
+	// TODO
+
+	return nil, fmt.Errorf("unimplemented")
+}
