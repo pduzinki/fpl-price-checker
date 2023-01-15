9b9b3ddae7e2634543252ef4001063a423357c69
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2023-01-15 21:59:23 +0100

    add get-report command

---
+++ pkg/services/get/get.go
@@ -0,0 +1,61 @@
+package get
+
+import (
+	"context"
+	"time"
+
+	"github.com/pduzinki/fpl-price-checker/pkg/domain"
+
+	"go.uber.org/multierr"
+)
+
+type PriceChangeReportGetter interface {
+	GetByDate(ctx context.Context, date string) (domain.PriceChangeReport, error)
+}
+
+type GetService struct {
+	rg PriceChangeReportGetter
+}
+
+func NewGetService(rg PriceChangeReportGetter) *GetService {
+	return &GetService{
+		rg: rg,
+	}
+}
+
+func (gs *GetService) GetLatestReport(ctx context.Context) (domain.PriceChangeReport, error) {
+	todaysDate := time.Now().Format(domain.DateFormat)
+	yesterdaysDate := time.Now().Add(-24 * time.Hour).Format(domain.DateFormat)
+
+	var errors error
+
+	// NOTE: FPL price changes usually occur around 1:30am GMT, so there's a time gap,
+	// where there's no "today's" report just yet. in that case, latest will be a report from the day before.
+
+	report, err := gs.rg.GetByDate(ctx, todaysDate)
+	if err == nil {
+		return report, nil
+	}
+	errors = multierr.Append(errors, err)
+
+	report, err = gs.rg.GetByDate(ctx, yesterdaysDate)
+	if err == nil {
+		return report, nil
+	}
+	errors = multierr.Append(errors, err)
+
+	return domain.PriceChangeReport{}, errors
+}
+
+func (gs *GetService) GetReportByDate(ctx context.Context, date string) (domain.PriceChangeReport, error) {
+	if err := domain.ParseDate(date); err != nil {
+		return domain.PriceChangeReport{}, err
+	}
+
+	report, err := gs.rg.GetByDate(ctx, date)
+	if err != nil {
+		return domain.PriceChangeReport{}, err
+	}
+
+	return report, nil
+}
