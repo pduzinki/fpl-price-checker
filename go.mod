e51ed1c61bb57b7f9cfce3d21e18b332a5a1b1fc
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2023-01-15 10:45:02 +0100

    add fs.PriceReportRepository tests

--- go.mod
+++ go.mod
@@ -7,14 +7,17 @@ require (
 	github.com/magefile/mage v1.14.0
 	github.com/rs/zerolog v1.28.0
 	github.com/spf13/cobra v1.6.1
+	github.com/stretchr/testify v1.8.1
 	go.uber.org/multierr v1.9.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
@@ -23,4 +26,5 @@ require (
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/storage/fs/pricereport_test.go
+++ pkg/storage/fs/pricereport_test.go
@@ -1,3 +1,115 @@
 package fs
 
-// TODO add later
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/pduzinki/fpl-price-checker/pkg/domain"
+	"github.com/pduzinki/fpl-price-checker/pkg/storage"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type PriceReportRepositoryTestSuite struct {
+	suite.Suite
+	folderPath string
+	repo       *PriceReportRepository
+}
+
+func (suite *PriceReportRepositoryTestSuite) SetupSuite() {
+	suite.folderPath = "./tmp_price_report_repository_test_data/"
+
+	err := os.MkdirAll(suite.folderPath, 0755)
+	suite.NoError(err)
+
+	repo, err := NewPriceReportRepository(suite.folderPath)
+	suite.NoError(err)
+
+	suite.repo = repo
+}
+
+func (suite *PriceReportRepositoryTestSuite) TearDownSuite() {
+	err := os.RemoveAll(suite.folderPath)
+	suite.NoError(err)
+}
+
+func TestPriceReportTestSuite(t *testing.T) {
+	suite.Run(t, new(PriceReportRepositoryTestSuite))
+}
+
+func (suite *PriceReportRepositoryTestSuite) TestPriceReportAddAndGetByDate() {
+	ctx := context.Background()
+
+	date := "1999-06-24"
+	report := domain.PriceChangeReport{
+		Date: date,
+		Records: []domain.Record{
+			{
+				Name:        "Kane",
+				OldPrice:    "12.2",
+				NewPrice:    "12.3",
+				Description: "rise",
+			},
+		},
+	}
+
+	err := suite.repo.Add(ctx, date, report)
+	suite.NoError(err)
+
+	gotReport, err := suite.repo.GetByDate(ctx, date)
+	suite.NoError(err)
+	suite.EqualValues(report, gotReport)
+}
+
+func (suite *PriceReportRepositoryTestSuite) TestPriceReportAddDuplicate() {
+	ctx := context.Background()
+
+	date := "1999-06-25"
+	report := domain.PriceChangeReport{
+		Date: date,
+		Records: []domain.Record{
+			{
+				Name:        "Salah",
+				OldPrice:    "12.2",
+				NewPrice:    "12.1",
+				Description: "drop",
+			},
+		},
+	}
+
+	err := suite.repo.Add(ctx, date, report)
+	suite.NoError(err)
+
+	err = suite.repo.Add(ctx, date, report)
+	suite.ErrorIs(err, storage.ErrDataAlreadyExists)
+}
+
+func (suite *PriceReportRepositoryTestSuite) TestPriceReportGetNonExistentEntry() {
+	ctx := context.Background()
+
+	date := "1999-06-26"
+	report, err := suite.repo.GetByDate(ctx, date)
+	suite.ErrorIs(err, storage.ErrDataNotFound)
+	suite.EqualValues(report, domain.PriceChangeReport{})
+}
+
+func (suite *PriceReportRepositoryTestSuite) TestPriceReportAddWithIncorrectlyFormattedDate() {
+	ctx := context.Background()
+
+	date := "not-even-a-date"
+	report := domain.PriceChangeReport{
+		Date: date,
+		Records: []domain.Record{
+			{
+				Name:        "Kane",
+				OldPrice:    "12.2",
+				NewPrice:    "12.3",
+				Description: "rise",
+			},
+		},
+	}
+
+	err := suite.repo.Add(ctx, date, report)
+	suite.Error(err)
+}
