9122e1ad1746dfa4110925e46041ca8367b25833
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2023-04-06 21:36:53 +0200

    add changes

--- tf/main.tf
+++ tf/main.tf
@@ -20,4 +20,51 @@ resource "aws_s3_object" "lambda_fetch" {
 
   key    = "fetch.zip"
   source = "${path.module}/../build/lambdas/fetch.zip"
+
+  etag = filemd5("${path.module}/../build/lambdas/fetch.zip")
+}
+
+resource "aws_lambda_function" "fetch" {
+  function_name = "fetch_tf"
+
+  s3_bucket = aws_s3_bucket.lambda_storage_bucket.id
+  s3_key    = aws_s3_object.lambda_fetch.key
+
+  runtime = "go1.x"
+  handler = "fetch"
+
+  # source_code_hash = 
+
+  role = aws_iam_role.lambda_fetch_exec.arn
+
+  environment {
+    variables = {
+      AWS_ID        = "${var.AWS_ID}"
+      AWS_S3_BUCKET = "${var.AWS_S3_BUCKET}"
+      AWS_SECRET    = "${var.AWS_SECRET}"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "fetch" {
+  name = "/aws/lambda/${aws_lambda_function.fetch.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_fetch_exec" {
+  name = "lambda_fetch"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
 }
