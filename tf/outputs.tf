d3cbe64b7658b2c0c553f15a759f2c30f622482b
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2023-04-16 20:32:48 +0200

    update tf output

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,5 +1,11 @@
 output "fpc_lambda_zip_storage_bucket_name" {
   description = "name of the s3 bucket used to store lambdas zips."
 
-  value = random_pet.fpc_lambda_zip_storage_bucket_name
+  value = random_pet.fpc_lambda_zip_storage_bucket_name.id
+}
+
+output "fpc_api_gateway_get_latest_report" {
+  description = "url of the fpc api gateway /latest endpoint"
+
+  value =format("%s%s", aws_apigatewayv2_stage.fpc_api_gateway_stage.invoke_url, split(" ", aws_apigatewayv2_route.fpc_get_latest.route_key)[1])
 }
